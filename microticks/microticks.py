6aaff4364802c1d824dd28ba3363d001508b865a
Dan Michael O. Heggø <danmichaelo@gmail.com>
2017-08-25 18:11:26 +0200

    Add cache control

--- microticks/microticks.py
+++ microticks/microticks.py
@@ -21,6 +21,15 @@ from .database import Database
 
 app = Flask(__name__)
 
+
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 # Config defaults
 app.config['FLASK_LOG_LEVEL'] = 'INFO'
 
